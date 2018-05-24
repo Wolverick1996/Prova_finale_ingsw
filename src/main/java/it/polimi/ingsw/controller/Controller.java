eea6a4b012f82d6ea66a76f022bf511a02b362b7
Ducolo <matteo1.ducoli@mail.polimi.it>
2018-05-24 16:04:53 +0200

    Controller started. Temp classes created to preserve work on network

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -1,9 +1,10 @@
 package it.polimi.ingsw.controller;
 
+import com.sun.org.apache.xerces.internal.xs.StringList;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Table;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Controller {
 
@@ -11,25 +12,37 @@ public class Controller {
     NOTE:
  */
 
-    public boolean startGame(ArrayList<Player> players){
+    public static Table startGame(List<String> nicknames, Temp_Lobby lobby){
 
         try {
 
-            System.out.println("Creating players ... ");
-            Table table = new Table(players.size());
-            System.out.println("OK");
+            lobby.broadcast("Creating table ... ");
+            Table table = new Table(nicknames.size());
+            lobby.broadcast("OK");
+            lobby.broadcast("This are the players: ");
+            for(String player:nicknames){lobby.broadcast(player);}
+            lobby.broadcast("Setting players ...");
+            table.setPlayers(nicknames);
+            lobby.broadcast("OK");
+            //TODO: Implementation of prep phase
+            lobby.broadcast("Game is ready");
+
+            lobby.broadcast("Printing reserve...");
+            for (int i=0; i<nicknames.size()*2 + 1; i++){lobby.broadcast(table.checkDiceFromReserve(i));}
+            //TODO: print status of game
+
+            return table;
 
         }catch (NullPointerException n){
             System.out.println("Null Pointer exception");
             n.printStackTrace();
-            return false;
+            return null;
         }
         catch (Exception e){
             System.out.println("Exception caught");
             e.printStackTrace();
-            return false;
+            return null;
         }
 
-        return true;
     }
 }
---
+++ src/main/java/it/polimi/ingsw/controller/Temp_Lobby.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.controller;
+
+import java.util.ArrayList;
+
+public class Temp_Lobby {
+
+    //***************************//
+    //        Attributes         //
+    //***************************//
+
+
+    public Temp_Lobby(){
+
+    }
+    //***************************//
+    //         Methods           //
+    //***************************//
+
+    public void broadcast(Object message){
+        System.out.println(message);
+    }
+}
