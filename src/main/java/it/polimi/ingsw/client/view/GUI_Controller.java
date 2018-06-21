eca638470fc9fb40251bf935d6699639bf1379ad
Riccardo <riccardo1.corona@mail.polimi.it>
2018-06-21 17:27:46 +0200

    Realized a first draft of the game table using JavaFX and SceneBuilder

---
+++ src/main/java/it/polimi/ingsw/client/view/GUI_Controller.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.client.view;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GUI_Controller implements Initializable{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
+}
---
+++ src/main/java/it/polimi/ingsw/client/view/GUI_Main.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.client.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUI_Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/GUI.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 1900, 968));
+        primaryStage.show();
+    }
+
+}
