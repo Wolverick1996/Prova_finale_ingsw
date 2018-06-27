cc33c133755291c2edad3ea2466b4ab3c3e5b59b
Riccardo <riccardo1.corona@mail.polimi.it>
2018-06-27 16:44:36 +0200

    GUI: Realized first 3 screens (without actions) and linked one to each other

--- src/main/java/it/polimi/ingsw/client/view/GUI_Controller.java
+++ src/main/java/it/polimi/ingsw/client/view/GUI_Controller.java
@@ -1,14 +1,41 @@
 package it.polimi.ingsw.client.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class GUI_Controller implements Initializable{
+public class First_Controller implements Initializable {
+
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private AnchorPane pane1;
+    @FXML
+    private AnchorPane pane2;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View is now loaded!");
     }
+
+    @FXML
+    private void loadSecond(ActionEvent event) throws IOException {
+        pane1 = FXMLLoader.load(getClass().getResource("/FXML/login.fxml"));
+        //The following command is used to replace the first window content with the second
+        rootPane.getChildren().setAll(pane1);
+    }
+
+    @FXML
+    private void loadThird(ActionEvent event) throws IOException {
+        pane2 = FXMLLoader.load(getClass().getResource("/FXML/lobby.fxml"));
+        pane1.getChildren().setAll(pane2);
+    }
+
 
 }
