4b660496373d52e75ccb1980b5856095d958ad4b
Ducolo <matteo1.ducoli@mail.polimi.it>
2018-07-08 15:57:03 +0200

    another Tool fix and empty-roundtrack check added

--- src/main/java/it/polimi/ingsw/client/view/GameController.java
+++ src/main/java/it/polimi/ingsw/client/view/GameController.java
@@ -148,6 +148,8 @@ public class GameController {
         }
     };
 
+    ArrayList getRoundtrackIMG() { return roundtrackIMG; }
+
     static synchronized void highlightGrid(boolean on){
         gridEffectOn = on;
     }
